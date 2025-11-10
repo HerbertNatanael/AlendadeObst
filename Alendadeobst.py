701d27dcfea41422caa5fb4325cae18130b79276
nome <herbertnatanaeldecastroveras@gmail.com>
2025-11-10 15:19:33 -0300

    erro

--- Alendadeobst.py
+++ Alendadeobst.py
@@ -1,3 +1,10 @@
 import pygame 
 pygame.init() #inicializa o pygame
-surf = pygame.display.set_mode((400,400)) #cria a janela do jogo
+surf = pygame.display.set_mode(400,400) #cria a janela do jogo
+surf.fill(0,0,0) #pinta a janela de preto
+pygame.display.update() #atualiza a janela do jogo
+
+while True: 
+    eventos = pygame.event.get()
+    if eventos:
+        print(eventos)
